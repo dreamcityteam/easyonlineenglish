d1b4d479d2d79c108f2c1881ba681a63b431a59a
Brewyn Espinal <brewyn@outlook.com>
2024-05-23 13:56:30 -0400

    Brewyn | Espinal Fixed styles and added pronunciation to the library 23-05-2024

--- frontend/src/pages/Student/Library/index.tsx
+++ frontend/src/pages/Student/Library/index.tsx
@@ -1,17 +1,19 @@
 import React, { useContext, useEffect, useState } from 'react';
 import Table from '../../../components/Table';
-import Sound from '../../../components/Sound';
 import style from './style.module.sass';
 import { SET_LIBRARY } from '../../../global/state/actionTypes';
 import { getData } from '../../../tools/function';
 import context from '../../../global/state/context';
 import { LibraryCache, LibraryContent } from '../../../global/state/type';
+import Speech from '../../../components/Speech';
+import { Item } from './type';
 
 const Library: React.FC = (): JSX.Element => {
   const [data, setData] = useState<LibraryCache>([]);
   const [content, setContent] = useState<LibraryContent[]>([]);
   const [tabIndex, setTabIndex] = useState<number>(0);
   const [{ libraryCache }, dispatch] = useContext(context);
+  const [speech, setSpeech] = useState<{ [key: string]: boolean; }>({});
 
   useEffect(() => {
     saveLibraryData();
@@ -44,6 +46,9 @@ const Library: React.FC = (): JSX.Element => {
     setTabIndex(index);
   }
 
+  const onCheck = (isCorrect: boolean, englishWord: string): void =>
+    setSpeech((currentState) => ({ ...currentState, [englishWord]: isCorrect }));
+
   return (
     <section className={style.vocabularies}>
       <header className={style.vocabularies__header}>
@@ -65,36 +70,47 @@ const Library: React.FC = (): JSX.Element => {
             ))}
           </ul>
         </aside>
-        <Table
-          data={content}
-          style={style}
-          custom={{
-            _id: {
-              avoid: true
-            },
-            englishWord: {
-              value: 'Ingles'
-            },
-            spanishTranslation: {
-              value: 'Español'
-            },
-            audioUrl: {
-              value: 'Audio',
-              render: (value: string): JSX.Element =>
-                <Sound src={value} style={style} />
-            },
-            imageUrl: {
-              value: 'Referencia',
-              render: (value: string): JSX.Element =>
-                <img
-                  alt="vocabulary image"
-                  className={style.table__image}
-                  src={value}
-                  loading="lazy"
-                />
-            },
-          }}
-        />
+        <div className={style.vocabularies__table}>
+          <Table
+            data={content}
+            style={style}
+            custom={{
+              _id: { avoid: true },
+              englishWord: { value: 'Ingles' },
+              spanishTranslation: { value: 'Español' },
+              audioUrl: {
+                value: 'Pronunciación',
+                render: (value: string, item: Item): JSX.Element =>
+                  <Speech
+                    audioUrl={value}
+                    onCheck={(isCorrect: boolean) => onCheck(isCorrect, item.englishWord)}
+                    word={item.englishWord}
+                  />
+              },
+              imageUrl: {
+                value: 'Referencia',
+                render: (value: string, item: Item): JSX.Element => (
+                  <div className={style.table__image_container}>
+                    <img
+                      alt="vocabulary image"
+                      className={style.table__image}
+                      loading="lazy"
+                      src={value}
+                    />
+                    {typeof speech[item.englishWord] !== 'undefined' && (
+                      <span
+                        className={style.table__feedback}
+                        style={{ background: speech[item.englishWord] ? '#4caf50' : '#f44336' }}
+                      >
+                        {speech[item.englishWord] ? 'Correcto' : 'Incorrecto'}
+                      </span>
+                    )}
+                  </div>
+                )
+              },
+            }}
+          />
+        </div>
       </div>
     </section>
   );
