aa5fa3ab0e6cbcdacd5b2b11134d35610efd9c14
Brewyn Espinal <brewyn@outlook.com>
2024-06-04 16:42:37 -0400

    Brewyn | Espinal Fixed style invoice 04-06-2024

--- backend/api/payment/function.js
+++ backend/api/payment/function.js
@@ -44,48 +44,49 @@ const formatDate = () => {
 }
 
 const getMessage = ({ name, phone, description, price, total }) => `
-  <div style="box-sizing: border-box; padding: 20px; font-family: arial; width: 100%; overflow: hidden;">
-    <header style="align-items: center; border-bottom: solid #598da6 2px; display: flex; justify-content: space-between; width: 100%;">
-      <img src="https://easyonlineenglish-eight.vercel.app/images/logo.svg" alt="logo">
-      <h2 style="color: #06609e; font-size: 60px;">FACTURA</h2>
-    </header>
-    <div style="display: flex; padding-bottom: 20px; justify-content: space-between; margin-top: 20px; width: 100%;">
-      <div style="line-height: 1.5;">
-        <div>
-          <strong>Datos del cliente</strong>
+  <div style="box-sizing: border-box; padding: 20px; font-family: arial; width: 100%; color: black; display: flex; justify-content: center;">
+    <div style="width: 600px;">
+      <header style="align-items: center; border-bottom: solid #598da6 2px; display: flex; justify-content: space-between; width: 100%;">
+        <img src="https://easyonlineenglish.com/wp-content/uploads/2023/12/Logo-Demo-12.png" alt="logo" style="width: 252px;">
+        <h2 style="color: #06609e; font-size: 60px;">FACTURA</h2>
+      </header>
+      <div style="display: flex; padding-bottom: 20px; justify-content: space-between; margin-top: 20px; width: 100%; justify-content: space-between;">
+        <div style="line-height: 1.5;">
+          <div>
+            <strong>Datos del cliente</strong>
+          </div>
+          <div style="text-transform: capitalize;">
+            <span>${name}</span>
+          </div>
+          <div>
+            <span>${phone}</span>
+          </div>
         </div>
-        <div style="text-transform: capitalize;">
-          <span>${name}</span>
-        </div>
         <div>
-          <span>${phone}</span>
+          <div>
+            <span>${formatDate()}</span>
+          </div>
         </div>
       </div>
-      <div>
-        <div>
-          <span>${formatDate()}</span>
-        </div>
-      </div>
+      <table style="border-collapse: collapse; margin-top: 50px; width: 100%;">
+        <thead>
+          <tr>
+            <th style="text-align: left; border-bottom: solid #598da6 2px; padding: 20px 0;">Descripción</th>
+            <th style="text-align: center; border-bottom: solid #598da6 2px; padding: 20px 0;">Precio</th>
+            <th style="text-align: right; border-bottom: solid #598da6 2px; padding: 20px 0;">Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td style="text-align: left; border-bottom: solid #598da6 2px; padding: 20px 0;">${description}</td>
+            <td style="text-align: center; border-bottom: solid #598da6 2px; padding: 20px 0;">${price}</td>
+            <td style="text-align: right; border-bottom: solid #598da6 2px; padding: 20px 0;">${total}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-    <table style="border-collapse: collapse; margin-top: 50px; width: 100%;">
-      <thead>
-        <tr>
-          <th style="text-align: left; border-bottom: solid #598da6 2px; padding: 20px 0;">Descripción</th>
-          <th style="text-align: center; border-bottom: solid #598da6 2px; padding: 20px 0;">Precio</th>
-          <th style="text-align: right; border-bottom: solid #598da6 2px; padding: 20px 0;">Total</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td style="text-align: left; border-bottom: solid #598da6 2px; padding: 20px 0;">${description}</td>
-          <td style="text-align: center; border-bottom: solid #598da6 2px; padding: 20px 0;">${price}</td>
-          <td style="text-align: right; border-bottom: solid #598da6 2px; padding: 20px 0;">${total}</td>
-        </tr>
-      </tbody>
-    </table>
   </div>
 `;
-
 
 const getDurationInMonth = (durationInMonth) => {
   const currentDate = new Date();
