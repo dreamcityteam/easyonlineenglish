b340d0c7a6f9d06bc17003813bcc69a721484263
Brewyn Espinal <brewyn@outlook.com>
2024-07-04 10:02:17 -0400

    Brewyn | Espinal fixed: styles course and remove cache 04-07-2024

--- backend/api/index.js
+++ backend/api/index.js
@@ -28,6 +28,13 @@ app.use(express.urlencoded({ limit: '10mb', extended: false }));
 app.use(middlewareToken);
 app.use('/api/v1', routers);
 
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  next();
+});
+
 app.use((_req, res, next) => {
   res.setHeader('Content-Security-Policy', "img-src 'self' data: *");
   next();
