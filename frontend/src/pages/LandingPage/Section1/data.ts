9c414d0b0d2d837130446447ecb4e9ba169b5f54
Brewyn Espinal <brewyn@outlook.com>
2024-08-21 09:42:00 -0400

    Brewyn | Espinal Fixed home page images 21-08-2024

--- frontend/src/pages/LandingPage/Section1/data.ts
+++ frontend/src/pages/LandingPage/Section1/data.ts
@@ -3,15 +3,15 @@ import { Image } from './type';
 const images: Image[] = [
   {
     alt: 'Worman watching a her computer',
-    path: 'home/woman-AFNvRcHab3hQiffhtGDblK0rxadeHJ.avif'
+    path: 'home/woman-tEgLIU2QggCPUaU40af0zyXHg4I2I4.jpg'
   },
   {
     alt: 'Man looking at his phone',
-    path: 'home/man-1-WhsFiCktY3J4elInslhpzIA0VORqTH.avif'
+    path: 'home/man-1-JtkqXPZq0XPgHScyNIQSdWJR6d4OyX.png'
   },
   {
     alt: 'Man using a table',
-    path: 'home/man-2-Ru54AicGgCO86Hnhwv9E4xQi8lpeGt.avif'
+    path: 'home/man-2-fKfa5j8IbR2hAwlMuFSX4DEfg2zhwB.png'
   }
 ];
 
