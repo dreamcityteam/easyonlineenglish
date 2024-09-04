592d51891347f2dd14495fac286013899c4709ff
Brewyn Espinal <brewyn@outlook.com>
2024-09-04 17:18:37 -0400

    Brewyn | Espinal added icons payment 04-09-2024

--- frontend/src/components/Footer/data.ts
+++ frontend/src/components/Footer/data.ts
@@ -1,7 +1,9 @@
 import { NamePath } from './type';
 
 const creditCards: NamePath[] = [
+  { name: 'Visa', path: 'thumbnail_Visa-Logo-2005-jt9tImarSlu1HhDg3UbAlE58FWRwo8.jpg' },
   { name: 'Visa secure', path: 'thumbnail_visa-secure_blu_2021%20-%20Copy%20-%20Cop1-d9DwAmWhPlzUQQCndKFKJFeGNk52FQ.png' },
+  { name: 'Mastercard', path: 'icons/mastercard-bzpdtN0K2WFVKGMhkSSycvdwAb6sKJ.png' },
   { name: 'Mastercard check id', path: 'mastercardidentitycheck-mPCEmnvAXxp1pS0l4jBXdmhoHurKL5.png' },
   { name: 'Paypal', path: 'icons/paypal-lMxavmkWSYEQ8Dc5UFRMKO2ns0QLpb.png' },
 ];
