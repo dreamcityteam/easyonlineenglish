d7ec395df06562b629cb26423e9bec7c2ddb615a
Brewyn Espinal <brewyn@outlook.com>
2024-09-12 16:01:04 -0400

    Brewyn | Espinal Enhance tutorial 12-09-2024

--- frontend/src/pages/Student/Course/Aside/index.tsx
+++ frontend/src/pages/Student/Course/Aside/index.tsx
@@ -79,13 +79,15 @@ const Aside: React.FC<Props> = ({
   };
 
   const handleStepChange = async (data: any): Promise<void> => {
-    if (data.index === 3) {
-      setSpotlightWidth('205px');
-    } else {
-      setSpotlightWidth('');
+    const { step, action } = data;
+    const element = document.querySelector(step.target);
+
+
+    if (element && step.target !== '#tidio-chat-iframe') {
+      element.style.cssText = 'position: relative; z-index: 2;';
     }
 
-    if (data.action === 'reset') {
+    if (action === 'reset') {
       Cookie.set(TUTORIAL, 'true');
       await send({ api: 'tutorial' }).patch();
     }
@@ -100,42 +102,188 @@ const Aside: React.FC<Props> = ({
         showProgress
         showSkipButton
         callback={handleStepChange}
+        spotlightClicks={true}
         steps={[
           {
             target: `.${style.aside__button}`,
-            content: <h2 className={style.tutorial__title}>Este es el menú de navigación del curso.</h2>,
+            content:
+              (
+                <div className={style.tutorial__title}>
+                  <header>
+                    <h2>Menú de navegación</h2>
+                  </header>
+                  <p>
+                    Aquí podrás ver cuantas lecciones has hecho en el curso                </p>
+                </div>
+              ),
             disableBeacon: true,
             disableScrolling: true,
           },
           {
             target: `.${courseStyle.course__englishWord}`,
-            content: <h2 className={style.tutorial__title}>Esta es la palabra que vas a aprender. Dale clic y la escucharás en dos velocidades.</h2>,
+            content:
+              (
+                <div className={style.tutorial__title}>
+                  <header>
+                    <h2>Palabra Objetivo</h2>
+                  </header>
+                  <p>
+                    Esta es la palabra que vas a aprender. Puedes darle "clic" las veces que quieras para practicarla.
+                  </p>
+                </div>
+              ),
             disableBeacon: true,
             disableScrolling: true,
 
           },
           {
             target: `.${courseStyle.course__spanishTranslation}`,
-            content: <h2 className={style.tutorial__title}>Esta es la trauducción al español de la palabra a aprender.</h2>,
+            content:
+              (
+                <div className={style.tutorial__title}>
+                  <header>
+                    <h2>Palabra Objectivo en Español</h2>
+                  </header>
+                  <p>
+                    Aquí verás el o los distintos significados en español de la palabra objetivo.
+                  </p>
+                </div>
+              ),
+            disableBeacon: true,
+            disableScrolling: true,
+
+          },
+          {
+            target: `.${courseStyle.course__progress}`,
+            content:
+              (
+                <div className={style.tutorial__title}>
+                  <header>
+                    <h2>Barra de Progreso</h2>
+                  </header>
+                  <p>
+                    Esta barra te indicará cuanto has avanzado en la palabra objetivo.                </p>
+                </div>
+              ),
             disableBeacon: true,
             disableScrolling: true,
 
           },
           {
             target: `.${courseStyle.course__content_text} .${courseStyle.course__text_grandient}`,
-            content: <h2 className={style.tutorial__title}>Esta es la palabra o la oración a aprender. Haz clic en ella y la escucharás en dos velocidades. </h2>,
+            content:
+              (
+                <div className={style.tutorial__title}>
+                  <header>
+                    <h2>Frase u Oración</h2>
+                  </header>
+                  <p>
+                    Aquí podrás practicar la palabra objetivo en una frase u oración. Dale "clic" y la escucharás las veces que quieras. Este audio tiene dos velocidades de reproducción, una lenta y una más rápida.
+                  </p>
+                </div>
+              ),
+            disableBeacon: true
+          },
+
+          {
+            target: `.${courseStyle.course__content_text} .${courseStyle.course__feedback}`,
+            content:
+              (
+                <div className={style.tutorial__title}>
+                  <header>
+                    <h2>Frase u Oración Práctica en Español</h2>
+                  </header>
+                  <p>
+                    Esta imagen te ayudará a intepretar y entender en contexto la frase u oración que está practicando.
+                  </p>
+                </div>
+              ),
+            disableBeacon: true
+          },
+          {
+            target: `.${courseStyle.course__content_text}:last-child > div`,
+            content:
+              (
+                <div className={style.tutorial__title}>
+                  <header>
+                    <h2>Frase u Oración Práctica en Español</h2>
+                  </header>
+                  <p>
+                    Aquí te presentamos la frase u oración en español para un mejor entendimiento.
+                  </p>
+                </div>
+              ),
+            disableBeacon: true
+          },
+          {
+            target: `.${courseStyle.course__feedback} img`,
+            content:
+              (
+                <div className={style.tutorial__title}>
+                  <header>
+                    <h2>Imagen Referencial</h2>
+                  </header>
+                  <p>
+                    Esta imagen te ayudará a interpretar y entender en contexto la frase u oración que estás practicando.
+                  </p>
+                </div>
+              ),
+            disableBeacon: true
+          },
+          {
+            target: `.${courseStyle.course__arrow}`,
+            content: (
+              <div className={style.tutorial__title}>
+                <header>
+                  <h2>Botón de Regreso</h2>
+                </header>
+                <p>
+                  Te permite ir hacia atrás en el curso.
+                </p>
+              </div>
+            ),
+            disableBeacon: true
+          },
+          {
+            target: `.${courseStyle.course__arrow}:last-child`,
+            content: (
+              <div className={style.tutorial__title}>
+                <header>
+                  <h2>Botón de Avance</h2>
+                </header>
+                <p>
+                  Te permite continuar a la siguiente práctica.
+                </p>
+              </div>
+            ),
             disableBeacon: true
           },
           {
             target: `.${speechStyle.speech}`,
-            content: <h2 className={style.tutorial__title}>Haz clic y escucharás la palabra u oración, Después del sonido puedes pronunciarla. Recuerda repetir cada palabra lentamente.</h2>,
+            content: (
+              <div className={style.tutorial__title}>
+                <header>
+                  <h2>Botón para escuchar y repetir</h2>
+                </header>
+                <p>
+                  Al presionar este botón escucharás el audio del texto y repetirás lo que escuchaste después del sonido.
+                </p>
+              </div>
+            ),
             disableBeacon: true
           },
           {
             target: '#tidio-chat-iframe',
-            content: <h2 className={style.tutorial__title}>
-             Ponte en contacto con nuestro esquipo de servicio al cliente en caso de que tengas algún comentario que hacer. 
-            </h2>,
+            content: (
+              <div className={style.tutorial__title}>
+                <header>
+                  <h2>Servicio al Estudiante</h2>
+                </header>
+                <p>
+                  Al presionar este botón te podrás poner en contacto con nuestro equipo de atención al cliente. Recibirás una respuesta en un lapso no mayor de 24 horas.
+                </p>
+              </div>
+            ),
             disableBeacon: true
           }
         ]}
@@ -154,8 +302,9 @@ const Aside: React.FC<Props> = ({
             display: 'none',
           },
           overlay: {
+            backgroundColor: 'white',
             height: 'calc(103px + 100%)',
-            zIndex: 4,
+            zIndex: 1,
           },
           options: {
             primaryColor: '#0c97d6',
@@ -188,7 +337,7 @@ const Aside: React.FC<Props> = ({
             <span
               ref={indexLesson === currentWord.lesson ? currentFocusLessonRef : null}
               className={style.aside__lession_title}
-              style={{ backgroundColor: lessonIndex === indexLesson ? '#f3f3f3' : ''}}
+              style={{ backgroundColor: lessonIndex === indexLesson ? '#f3f3f3' : '' }}
             >
               {lesson?.title}
             </span>
