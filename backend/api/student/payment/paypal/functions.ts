7ea57ebdf9c45532bcaae8273db69a42e1c03913
Berwyn <brewyn@outlook.com>
2025-05-29 10:06:20 -0400

    added credit card

--- backend/api/student/payment/paypal/functions.ts
+++ backend/api/student/payment/paypal/functions.ts
@@ -36,18 +36,18 @@ export const createOrder = async (plan: 1 | 2 | 3) => {
               quantity: 1,
               unit_amount: {
                 currency_code: 'USD',
-                value: PAYMENT_METHOD[plan].AMOUNT
+                value: 1
               }
             }
           ],
 
           amount: {
             currency_code: 'USD',
-            value: PAYMENT_METHOD[plan].AMOUNT,
+            value: 1,
             breakdown: {
               item_total: {
                 currency_code: 'USD',
-                value: PAYMENT_METHOD[plan].AMOUNT
+                value: 1
               }
             }
           }
