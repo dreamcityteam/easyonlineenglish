faa7b2a222239e9090f7e06fccd9c5ccfd7c7bfd
Anthonchess <anthonchess@gmail.com>
2025-06-11 15:08:21 -0400

    cambios en testimonios

--- frontend/src/pages/LandingPage/Section4/data.ts
+++ frontend/src/pages/LandingPage/Section4/data.ts
@@ -2,24 +2,24 @@ import { Card } from './type';
 
 const cards: Card[] = [
   {
-    alt: 'Eva Elle\'s photo',
-    path: 'home/erick-garcia-yG28ChFK0SvOr8jfPTrLiSD2WurBxV.png',
-    userName: 'Erick García',
-    userTag: '@erickg01',
-    description: 'Me encanta la forma en que EasyOnlineEnglish.com enseña inglés. Todo está muy bien estructurado y es perfecto para alguien que quiere aprender sin complicaciones. He mejorado muchísimo en poco tiempo.',
+    alt: '',
+    path: '',
+    userName: 'Leandro Castillo',
+    userTag: '',
+    description: 'Easy Online English superó mis expectativas. Mejoré mi pronunciación desde la primera lección y gané confianza al poder hablar con un amigo extranjero sin miedo. Es práctico, fácil de usar y realmente efectivo. ',
   },
   {
-    alt: 'Guy Mccoy\'s photo',
-    path: 'home/juan-perez-aXBss1lDXOk9VJGo4WY3K4eG5xYEl3.png',
-    userName: 'Juan Pérez',
-    userTag: '@juanperez',
-    description: 'Había probado varios métodos para aprender inglés, pero nunca había encontrado uno tan efectivo como EasyOnlineEnglish.com. La flexibilidad de los planes y la claridad de las lecciones hacen que aprender sea un placer.',
+    alt: '',
+    path: '',
+    userName: 'Ángel Isidro',
+    userTag: '',
+    description: 'Easy Online English me ayudó a ganar confianza en el trabajo. Ahora puedo explicar ideas y entender términos técnicos en inglés sin problemas. Aprendí a usar correctamente palabras que antes decía mal, y ya no me confunden los menús o instrucciones del software. Fue un cambio total en cómo me comunico.',
   },
   {
-    alt: 'Kayla Ray\'s photo',
-    path: 'home/maria-rodriguez-VYjqTwH42oi24CVatrsQtdzAsfrM5i.jpg',
+    alt: '',
+    path: '',
     userName: 'María Rodríguez',
-    userTag: '@maríarodri',
+    userTag: '',
     description: 'Gracias a EasyOnlineEnglish.com, ahora puedo comunicarme en inglés con confianza. Los cursos son fáciles de seguir y realmente te ayudan a entender el idioma sin confusiones.',
   }
 ];
